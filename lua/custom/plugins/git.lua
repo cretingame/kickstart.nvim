00e2bff08e60c6eaf2421db50f44094423b7adc5
Arnaud Sautaux <arnaud.sautaux@mc-monitoring.com>
2024-10-28 08:59:34 +0100

    git key shortcut

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,3 +1,16 @@
 return {
-  { 'akinsho/git-conflict.nvim', version = '*', config = true },
+  {
+    'akinsho/git-conflict.nvim',
+    version = '*',
+    config = {
+      default_mappings = {
+        ours = 'o',
+        theirs = 't',
+        none = '0',
+        both = 'b',
+        next = 'n',
+        prev = 'p',
+      },
+    },
+  },
 }
